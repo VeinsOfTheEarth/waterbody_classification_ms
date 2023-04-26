ce2488773ed9f7c8bc2e95c82ea1f674f0eb7c47
jsta <jemma.stachelek@gmail.com>
2023-04-26 08:20:28 -0600

    planning

---
+++ wb_compare.py
@@ -0,0 +1,14 @@
+# hydrolakes
+# perl
+# glakes
+
+import os
+import glob
+
+data_folder = "C:/Vote Data"
+path_hydrolakes = data_folder + "/hydrolakes_data/HydroLAKES_polys_v10_shp/HydroLAKES_polys_v10.shp"
+path_perl = data_folder + ""
+
+os.path.exists(path_hydrolakes)
+
+# 66 deg 45 min N, 77 deg 55 min
