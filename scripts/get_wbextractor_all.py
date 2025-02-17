0d670529fa263de39481145f6e0f5a509c582813
Jemma Stachelek <jemma.stachelek@gmail.com>
2025-02-17 15:55:52 -0700

    towards overall fabric

--- scripts/get_wbextractor_all.py
+++ scripts/get_wbextractor_all.py
@@ -1,10 +1,18 @@
+# TODO: collect wb(s) from multiple wb_all aois
+import pandas as pd
 import geopandas as gpd
 
-# TODO: collect wb(s) from multiple wb_all aois
-
 # read selected aois from data/wb_all.zip
+path_zip = "data/wb_all.zip"
+aois = ["0670116", "0769611"]
+zipfiles = ["{}!data/{}/data/wb_all.gpkg".format(path_zip, aoi) for aoi in aois]
+wb_all = pd.concat([gpd.read_file(x) for x in zipfiles])
 
 
 def aoi_health_check():
     # check that the distribution and counts are not ridiculous
+    return None
+
+def make_fabric():
+    # merge wbs that intersect (or touch?) aoi boundaries
     return None
