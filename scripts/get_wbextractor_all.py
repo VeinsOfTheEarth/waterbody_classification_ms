cfc8ce7bda44fa821135c1cc961e75b9da33861e
Jemma Stachelek <jemma.stachelek@gmail.com>
2025-02-18 16:58:21 -0700

    stash progress

--- scripts/get_wbextractor_all.py
+++ scripts/get_wbextractor_all.py
@@ -1,5 +1,6 @@
 # TODO: collect wb(s) from multiple wb_all aois
 import sys
+import numpy as np
 import pandas as pd
 import geopandas as gpd
 
@@ -10,19 +11,28 @@ from src import utils
 path_zip = "data/wb_all.zip"
 aois = ["0670116", "0769611"]
 zipfiles = ["{}!data/{}/data/wb_all.gpkg".format(path_zip, aoi) for aoi in aois]
-wb_all = pd.concat(
+
+wb_all_bbox = pd.concat(
     [utils.make_bbox_gdf(gpd.read_file(x)) for x in zipfiles]
 ).reset_index(drop=True)
-wb_all["idx"] = aois
-wb_all.to_file("test_bbox.gpkg")
-
-# bnds_gdf = utils.make_bbox_gdf(wb_all)
-# bnds_gdf.to_file("test_bbox.gpkg")
+wb_all_bbox["idx"] = aois
+wb_all_bbox.to_file("test_bbox.gpkg")
 
 
-def aoi_health_check():
+def aoi_health_check(aoi, path_zip):
     # check that the distribution and counts are not ridiculous
-    return None
+    res = {"aoi": aoi}
+    path_in = "{}!data/{}/data/wb_all.gpkg".format(path_zip, aoi)
+    gdf = gpd.read_file(path_in)
+    
+    res.update({"npolys": gdf.shape[0]})
+    res.update({"median": round(np.quantile([x for x in gdf.area], 0.5), 2)})
+    res.update({"q1": round(np.quantile([x for x in gdf.area], 0.1), 2)})
+    
+    return res
+
+
+aoi_health_check(aois[0], path_zip=path_zip)
 
 
 def make_fabric():
