a70077ca6a490299af340516392165955ff6d08d
Jemma Stachelek <jemma.stachelek@gmail.com>
2025-02-19 16:47:58 -0700

    planning

--- Makefile
+++ Makefile
@@ -1,16 +1,18 @@
-.PHONY: all manuscript figures data_eval test
+.PHONY: all manuscript figures data_eval test data
 
 PDFCROP=pdfcrop.pl
 gpkgs ?= $(addprefix data/, $(addsuffix /data/wb_all.gpkg, $(shell cat data/aois.txt)))
 populates ?= $(addprefix data/, $(addsuffix /query_ids_quality.txt, $(shell cat to_transfer.txt)))
 preflights ?= $(addprefix data/, $(addsuffix /query_ids_quality.txt, $(shell cat preflight.txt)))
 
-# @echo $(gpkgs)
-test:	
-	@echo $(populates)
+all: manuscript figures data
 
-all: manuscript figures data_eval
+data: data/cubeSat_buffered_mask_tiles.gpkg
 
+data/cubeSat_buffered_mask_tiles.gpkg: data/CubeSat_Arctic_Boreal_LakeArea_1667/data/CubeSat_Buffered_Mask_Tiles.kmz
+	ogr2ogr $@ $<
+
+# this target is meant to be called manually, kicks off full pipeline
 data_eval: $(gpkgs)
 
 $(gpkgs): data/aois.txt
--- scripts/get_wbextractor_all.py
+++ scripts/get_wbextractor_all.py
@@ -7,16 +7,11 @@ import geopandas as gpd
 sys.path.append(".")
 from src import utils
 
+pd.set_option("display.max_columns", None)
+
 # read selected aois from data/wb_all.zip
 path_zip = "data/wb_all.zip"
 aois = ["0670116", "0769611"]
-zipfiles = ["{}!data/{}/data/wb_all.gpkg".format(path_zip, aoi) for aoi in aois]
-
-wb_all_bbox = pd.concat(
-    [utils.make_bbox_gdf(gpd.read_file(x)) for x in zipfiles]
-).reset_index(drop=True)
-wb_all_bbox["idx"] = aois
-wb_all_bbox.to_file("test_bbox.gpkg")
 
 
 def aoi_health_check(aoi, path_zip):
@@ -24,17 +19,49 @@ def aoi_health_check(aoi, path_zip):
     res = {"aoi": aoi}
     path_in = "{}!data/{}/data/wb_all.gpkg".format(path_zip, aoi)
     gdf = gpd.read_file(path_in)
-    
+
     res.update({"npolys": gdf.shape[0]})
     res.update({"median": round(np.quantile([x for x in gdf.area], 0.5), 2)})
     res.update({"q1": round(np.quantile([x for x in gdf.area], 0.1), 2)})
-    
-    return res
+
+    return {"stats": res}
 
 
-aoi_health_check(aois[0], path_zip=path_zip)
-
-
-def make_fabric():
+def make_fabric(aois, path_zip):
     # merge wbs that intersect (or touch?) aoi boundaries
+    zipfiles = ["{}!data/{}/data/wb_all.gpkg".format(path_zip, aoi) for aoi in aois]
+    wb_all = [(gpd.read_file(x)) for x in zipfiles]
+    for i in range(len(wb_all)):
+        wb_all[i]["idx"] = aois[i]
+    
+    wb_all_bbox = utils.make_bbox_gdf(pd.concat(wb_all).reset_index(drop=True))
+    aoi_boundaries = gpd.read_file("data/cubeSat_buffered_mask_tiles.gpkg")
+
+    breakpoint()
+    # TODO: limit the read of aoi_boundaries to wb_all_bbox bounds
+
+    # use wb_all centroids to find matching aoi_boundary
+
+    # split out wb(s) that touch aoi_boundary
+
+    # remove wb(s) below an area threshold
+
+    # ---
+
+    # get all pairs of touching aoi boundaries
+
+    # for each pair, evaluate those with an "on_boundary" flag for touching
+
+    # merge those that touch
+
+    # remove original wb(s) corresponding to each merge from parent wb_all members
+
+    # ---
+
+    # concat modified wb_all members and on_boundary merges
+
     return None
+
+
+aoi_health_check(aois[0], path_zip=path_zip)["stats"]
+make_fabric(aois, path_zip)
