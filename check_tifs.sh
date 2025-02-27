51bd69be44f8450f8f6199b4879e723bc1189485
Jemma Stachelek <jemma.stachelek@gmail.com>
2025-02-27 15:00:29 -0700

    macos fix

--- check_tifs.sh
+++ check_tifs.sh
@@ -2,10 +2,10 @@
 # print any workspace folders without tif files
 
 # gnu find
-find data -type d -regextype egrep -regex '.*/[0-9]{7}' > data_folders.txt
+# find data -type d -regextype egrep -regex '.*/[0-9]{7}' > data_folders.txt
 
 # # macos
-# find -E data -iregex '.*[0-9]{7}' > data_folders.txt
+find -E data -iregex '.*[0-9]{7}' -type d  > data_folders.txt
 
 for folder in `cat data_folders.txt`
 do
